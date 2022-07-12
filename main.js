f7521f07828604953545bf0eb6e61b46393cef4b
WowMuchDoge <97766806+WowMuchDoge@users.noreply.github.com>
2022-07-11 20:59:43 -0500

    Test 2

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const path = require('path')
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'))
+    res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 app.listen(port, () => {
